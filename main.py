2c4d5bef04878871eb26a70d4532e81aeed8ca61
Marina <glotova_marina@rambler.ru>
2023-12-18 22:45:06 +0500

    Круги случайного размера и случайного цвета.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import sys
 import random
 
-from PyQt5 import uic  # Импортируем uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from UI import Ui_MainWindow
 from PyQt5.QtGui import QPainter, QColor, QPen
